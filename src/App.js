cbd6043e49213f9168e0c92b68ee8a9e477e9c97
ivenckunas <ignas.venckunas@gmail.com>
2023-01-25 12:31:48 +0200

    master branch: changes deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import '../src/css/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import Nav from './components/Nav/Nav';
 
 function App() {
   const dispatch = useDispatch();
 
   return <div className='App'>
-    <div className="container">
-      <Nav />
-    </div>
 
   </div>;
 }
