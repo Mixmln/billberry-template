da187cf9a6a8b194485efa8eda583cab6f4c8231
Arturas <mixmln14@gmail.com>
2023-01-25 12:18:21 +0200

    bilberry init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import '../src/css/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+function App() {
+  const dispatch = useDispatch();
+
+  return <div className='App'></div>;
+}
+
+export default App;
