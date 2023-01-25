98b35c3fbb109555774af8b66c917ac59646a0d8
Arturas <mixmln14@gmail.com>
2023-01-25 13:07:57 +0200

    about section done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import '../src/css/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Nav from './components/Nav/Nav';
+import About from './components/About/About';
 
 function App() {
   const dispatch = useDispatch();
@@ -9,6 +10,8 @@ function App() {
   return <div className='App'>
     <div className="container">
       <Nav />
+      <About />
+      <h3>d</h3>
     </div>
   </div>;
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,26 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import '../About/about.css'
+
+function About() {
+  return (
+    <div className='about-section'>
+      <div className='about-text'>
+        <h4 className='about-title'>WE ARE BILBERRY</h4>
+        <p className='about-desc'>We design and develop websites, apps and digital experiences that help our clients grow, innovate, and transform. We listen, learn and understand before we build. We identify your goals together, then use our expertise to find that sweet spot of realistic and impactful.</p>
+      </div>
+      <Stack className='logos-flex container' direction='row' sx={{
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <img className='logo-pepsico' src="https://bilberrry.com/wp-content/uploads/2020/05/pepsico-dark.svg" alt="" />
+        <img className='logo-chipotle' src="https://bilberrry.com/wp-content/uploads/2020/05/chipotle-dark.svg" alt="" />
+        <img className='logo-delta' src="https://bilberrry.com/wp-content/uploads/2020/05/delta-dark.svg" alt="" />
+        <img className='logo-uni' src="https://bilberrry.com/wp-content/uploads/2020/05/university-light.svg" alt="" />
+        <img className='logo-gofoundme' src="https://bilberrry.com/wp-content/uploads/2022/08/gofundme.png" alt="" />
+      </Stack>
+    </div>
+  )
+}
+
+export default About
