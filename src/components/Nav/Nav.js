ce5f4e8141ec922fcb799c2a8c51840488d7487f
ivenckunas <ignas.venckunas@gmail.com>
2023-01-25 12:28:38 +0200

    nav bar added

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div>
+      <nav>
+        <img src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" alt="logo" />
+        <ul>
+          <li>projects</li>
+          <li>about us</li>
+          <li>contact us</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
