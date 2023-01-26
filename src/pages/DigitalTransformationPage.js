e0ef33ca1688416619fb6757c681a6a51b5a5b76
ivenckunas <ignas.venckunas@gmail.com>
2023-01-26 14:11:53 +0200

    digital page title, image

--- src/pages/DigitalTransformationPage.js
+++ src/pages/DigitalTransformationPage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero/Hero'
+import Nav from '../components/Nav/Nav'
 
 function DigitalTransformationPage() {
   return (
-    <div>DigitalTransformationPage</div>
+    <div className='App'>
+      <Nav />
+      <Hero title='MAKE IT HAPPEN STARTING NOW' mainImage='https://images.unsplash.com/photo-1429667947446-3c93a979b7e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' />
+    </div>
   )
 }
 
